67d6684f2737e11b2b9d43bb2d76306115ed992f
Sharjeel Riaz <sharjeelriazsh@gmail.com>
2022-10-19 19:34:55 +0500

    Updated repo

---
+++ Fundamentals/Importing Components/reactlab/src/App.js
@@ -0,0 +1,15 @@
+//This file won't work as its downloaded from the completed remote coursera lab server.
+//To make this work, you have to create a fresh pacakage using npm then make the necessary changes
+//to see the actual results.
+
+import Heading from "./Heading";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
