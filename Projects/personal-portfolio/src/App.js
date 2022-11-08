a966b44debd7d33ccbf7b94206b4509891e89493
Sharjeel Riaz <sharjeelriazsh@gmail.com>
2022-11-08 23:43:39 +0500

    Updated Repo

--- Projects/personal-portfolio/src/App.js
+++ Projects/personal-portfolio/src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./Components/NavBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
