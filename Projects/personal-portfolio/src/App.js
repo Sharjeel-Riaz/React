62b3372ea6be5f8d8ab4ae3a327699484e2ebb55
Sharjeel Riaz <sharjeelriazsh@gmail.com>
2022-11-09 18:22:23 +0500

    Updated Repo

--- Projects/personal-portfolio/src/App.js
+++ Projects/personal-portfolio/src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./Components/NavBar";
+import { Banner } from "./Components/Banner";
+import { Skills } from "./Components/Skills";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+      <Skills />
     </div>
   );
 }
