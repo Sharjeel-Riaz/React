7cb6c502cd53d5979e2171e44630bc71e1e556e9
Sharjeel Riaz <sharjeelriazsh@gmail.com>
2024-03-01 23:50:47 +0500

    Worked on Navbar/Footer.jsx

--- Projects/circuit-bazaar/components/Footer.jsx
+++ Projects/circuit-bazaar/components/Footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { AiFillInstagram, AiOutlineFacebook } from "react-icons/ai";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer-container">
+      <p>© 2024 - Present CircuitBazaar. All Rights Reserved.</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineFacebook />
+        <FaSquareXTwitter />
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
